261191951396d97ceb1d7927833ded440e842e2b
Paul Brady <pablo.brady@gmail.com>
2022-01-02 10:56:45 -0800

    Link

--- src/main.js
+++ src/main.js
@@ -4,5 +4,5 @@ import './index.css'
 document.querySelector('#app').innerHTML = `
   <h1 class="text-4xl pb-4">Hello Vanilla JS Vite! <span class="text-purple-500 font-bold">(with Tailwind v3 classes)</span></h1>
   <p><a href="https://vitejs.dev/guide/features.html" target="_blank" class="hover:text-blue-600 hover:font-bold">ViteJS Documentation</a></p>
-  <p><a href="https://tailwindcss.com/" target="_blank" class="hover:text-blue-600 hover:font-bold">TailwindCSS v3 Documentation</a></p>
+  <p><a href="https://tailwindcss.com/docs/theme" target="_blank" class="hover:text-blue-600 hover:font-bold">TailwindCSS v3 Documentation</a></p>
 `
