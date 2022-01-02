905dbd32d4b803ca3964c82ebdc197e66c439fa4
Paul Brady <pablo.brady@gmail.com>
2022-01-02 10:31:43 -0800

    JS Template Tweaks

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './style.css'
 import './index.css'
 
 document.querySelector('#app').innerHTML = `
-  <h1 class="text-4xl">Hello Vanilla Vite! <span class="text-purple-500">(PB REPO TEMPLATE)</span></h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  <h1 class="text-4xl pb-4">Hello Vanilla JS Vite! <span class="text-purple-500 font-bold">(with Tailwind v3 classes)</span></h1>
+  <p><a href="https://vitejs.dev/guide/features.html" target="_blank" class="hover:text-blue-600 hover:font-bold">ViteJS Documentation</a></p>
+  <p><a href="https://tailwindcss.com/" target="_blank" class="hover:text-blue-600 hover:font-bold">TailwindCSS v3 Documentation</a></p>
 `
