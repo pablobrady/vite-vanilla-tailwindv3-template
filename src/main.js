3ec06747aaf7b3c61fe763e3c765f0e11843f2d3
Paul Brady <pablo.brady@gmail.com>
2022-01-02 10:25:29 -0800

    Enable TailwindCSS v3 w/ PostCSS

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './style.css'
+import './index.css'
 
 document.querySelector('#app').innerHTML = `
-  <h1>Hello Vanilla Vite! <span class="text-purple-500">(PB REPO TEMPLATE)</span></h1>
+  <h1 class="text-4xl">Hello Vanilla Vite! <span class="text-purple-500">(PB REPO TEMPLATE)</span></h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsz,tsx}"
+  ],
   theme: {
     extend: {},
   },
